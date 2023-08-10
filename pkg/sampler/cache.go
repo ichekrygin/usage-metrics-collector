c62a1ee522fe68fc250a06b29da4f17eeb6eff25
ichekrygin <ichekrygin@apple.com>
2023-08-09 21:03:48 -0700

    Replace CadvisorNetworkStats with a local analog with reduced field set to further reduce memory footprint by only inlcuding fields needed for sample reporting.

--- pkg/sampler/cache.go
+++ pkg/sampler/cache.go
@@ -422,56 +422,56 @@ func (s *sampleCache) computeSampleDelta(last, sample *sampleInstant) {
 	sample.MemoryOOMKill = sample.CumulativeMemoryOOMKill - last.CumulativeMemoryOOMKill
 }
 
-//func populateCadvisorSummary(sr *sampleResult, dropFirstRecord bool) {
-//	if sr == nil {
-//		return
-//	}
-//	// Use networkSampleCount to track total sample count since not all samples may have cadvisor metrics.
-//	var (
-//		networkSamplesCount uint64
-//		networkRxBytes      uint64
-//		networkRxPackets    uint64
-//		networkRxErrors     uint64
-//		networkRxDropped    uint64
-//		networkTxBytes      uint64
-//		networkTxPackets    uint64
-//		networkTxErrors     uint64
-//		networkTxDropped    uint64
-//	)
-//
-//	for i := range sr.values {
-//		if i == 0 && dropFirstRecord {
-//			// Skip the first record for consistency with the `populateSummary`.
-//			continue
-//		}
-//		v := sr.values[i]
-//
-//		// Sum network metrics for all sample values, using stats.Timestamp value
-//		// to detect missing stats values, since all CAdvisorContainerStats have Timestamp value > 0.
-//		if v.CAdvisorContainerStats.Timestamp.Unix() > 0 {
-//			networkSamplesCount++
-//			networkRxBytes += v.CAdvisorContainerStats.Network.RxBytes
-//			networkRxPackets += v.CAdvisorContainerStats.Network.RxPackets
-//			networkRxErrors += v.CAdvisorContainerStats.Network.RxErrors
-//			networkRxDropped += v.CAdvisorContainerStats.Network.RxDropped
-//			networkTxBytes += v.CAdvisorContainerStats.Network.TxBytes
-//			networkTxPackets += v.CAdvisorContainerStats.Network.TxPackets
-//			networkTxErrors += v.CAdvisorContainerStats.Network.TxErrors
-//			networkTxDropped += v.CAdvisorContainerStats.Network.TxDropped
-//		}
-//	}
-//	// Follow the same suite as "MemoryBytes" for cadvisor metrics.
-//	if networkSamplesCount > 0 {
-//		sr.avg.CAdvisorContainerStats.Network.RxBytes = networkRxBytes / networkSamplesCount
-//		sr.avg.CAdvisorContainerStats.Network.RxPackets = networkRxPackets / networkSamplesCount
-//		sr.avg.CAdvisorContainerStats.Network.RxErrors = networkRxErrors / networkSamplesCount
-//		sr.avg.CAdvisorContainerStats.Network.RxDropped = networkRxDropped / networkSamplesCount
-//		sr.avg.CAdvisorContainerStats.Network.TxBytes = networkTxBytes / networkSamplesCount
-//		sr.avg.CAdvisorContainerStats.Network.TxPackets = networkTxPackets / networkSamplesCount
-//		sr.avg.CAdvisorContainerStats.Network.TxErrors = networkTxErrors / networkSamplesCount
-//		sr.avg.CAdvisorContainerStats.Network.TxDropped = networkTxDropped / networkSamplesCount
-//	}
-//}
+func populateCadvisorSummary(sr *sampleResult, dropFirstRecord bool) {
+	if sr == nil {
+		return
+	}
+	// Use networkSampleCount to track total sample count since not all samples may have cadvisor metrics.
+	var (
+		networkSamplesCount uint64
+		networkRxBytes      uint64
+		networkRxPackets    uint64
+		networkRxErrors     uint64
+		networkRxDropped    uint64
+		networkTxBytes      uint64
+		networkTxPackets    uint64
+		networkTxErrors     uint64
+		networkTxDropped    uint64
+	)
+
+	for i := range sr.values {
+		if i == 0 && dropFirstRecord {
+			// Skip the first record for consistency with the `populateSummary`.
+			continue
+		}
+		v := sr.values[i]
+
+		// Sum network metrics for all sample values, using stats.Timestamp value
+		// to detect missing stats values, since all CAdvisorContainerStats have Timestamp value > 0.
+		if v.CAdvisorNetworkStats.Timestamp.Unix() > 0 {
+			networkSamplesCount++
+			networkRxBytes += v.CAdvisorNetworkStats.RxBytes
+			networkRxPackets += v.CAdvisorNetworkStats.RxPackets
+			networkRxErrors += v.CAdvisorNetworkStats.RxErrors
+			networkRxDropped += v.CAdvisorNetworkStats.RxDropped
+			networkTxBytes += v.CAdvisorNetworkStats.TxBytes
+			networkTxPackets += v.CAdvisorNetworkStats.TxPackets
+			networkTxErrors += v.CAdvisorNetworkStats.TxErrors
+			networkTxDropped += v.CAdvisorNetworkStats.TxDropped
+		}
+	}
+	// Follow the same suite as "MemoryBytes" for cadvisor metrics.
+	if networkSamplesCount > 0 {
+		sr.avg.CAdvisorNetworkStats.RxBytes = networkRxBytes / networkSamplesCount
+		sr.avg.CAdvisorNetworkStats.RxPackets = networkRxPackets / networkSamplesCount
+		sr.avg.CAdvisorNetworkStats.RxErrors = networkRxErrors / networkSamplesCount
+		sr.avg.CAdvisorNetworkStats.RxDropped = networkRxDropped / networkSamplesCount
+		sr.avg.CAdvisorNetworkStats.TxBytes = networkTxBytes / networkSamplesCount
+		sr.avg.CAdvisorNetworkStats.TxPackets = networkTxPackets / networkSamplesCount
+		sr.avg.CAdvisorNetworkStats.TxErrors = networkTxErrors / networkSamplesCount
+		sr.avg.CAdvisorNetworkStats.TxDropped = networkTxDropped / networkSamplesCount
+	}
+}
 
 func (s *sampleCache) populateSummary(sr *sampleResult) {
 	count := int64(len(sr.values))
--- pkg/sampler/types.go
+++ pkg/sampler/types.go
@@ -17,8 +17,6 @@ package sampler
 import (
 	"time"
 
-	cadvisorv1 "github.com/google/cadvisor/info/v1"
-
 	"sigs.k8s.io/usage-metrics-collector/pkg/api/samplerserverv1alpha1"
 )
 
@@ -38,6 +36,26 @@ type ContainerKey struct {
 
 	// PodName is the name of the pod
 	PodName string
+}
+
+type cadvisorNetworkStats struct {
+	Timestamp time.Time
+	// Cumulative count of bytes received.
+	RxBytes uint64 `json:"rx_bytes"`
+	// Cumulative count of packets received.
+	RxPackets uint64 `json:"rx_packets"`
+	// Cumulative count of receive errors encountered.
+	RxErrors uint64 `json:"rx_errors"`
+	// Cumulative count of packets dropped while receiving.
+	RxDropped uint64 `json:"rx_dropped"`
+	// Cumulative count of bytes transmitted.
+	TxBytes uint64 `json:"tx_bytes"`
+	// Cumulative count of packets transmitted.
+	TxPackets uint64 `json:"tx_packets"`
+	// Cumulative count of transmit errors encountered.
+	TxErrors uint64 `json:"tx_errors"`
+	// Cumulative count of packets dropped while transmitting.
+	TxDropped uint64 `json:"tx_dropped"`
 }
 
 type sampleInstant struct {
@@ -76,7 +94,7 @@ type sampleInstant struct {
 	// Although, currently, we only collect and report network metrics,
 	// we [re]use ContainerStats as sample value holder since in the future
 	// we may choose to collect addition values (other than Network)
-	CAdvisorNetworkStats cadvisorv1.NetworkStats
+	CAdvisorNetworkStats cadvisorNetworkStats
 }
 
 // ContainerMetricType identifies a type of metrics that corresponds to a specific cgroups file
